b7c2f84cd346fdd9138a02cada8395afeea4a3f7
JEEVANANTHAN A <106263059+jeevananthan27@users.noreply.github.com>
2023-03-10 21:23:45 +0530

    Add files via upload

---
+++ Pyramids and Patterns.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+
+
+int main()
+
+
+{
+    
+
+int i,j;
+    
+
+int rows;
+    
+
+printf("Enter the no rows");
+    
+
+scanf("%d",&rows);
+   
+
+ for(i=1;i<=rows;i++){
+ 
+
+   for(j=1;j<=i;j++){
+   
+
+     printf("*");
+    
+
+    }
+   
+
+ printf("\n");
+  
+
+  }
+
+  
+
+  return 0;
+
+
+}
